b7646e4239740de64ea0296eae31433a82183f52
Kuromaru0530 <kuromary77@gmail.com>
2023-04-10 16:30:18 +0900

    プロジェクト ファイルを追加します。

---
+++ ITP1_8_D/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace ITP1_8_D
+{
+    class Program
+    {
+        static void Main()
+        {
+            var s = Console.ReadLine();
+            var p = Console.ReadLine();
+            var s2 = s+s;
+
+            if (s2.Contains(p)) Console.WriteLine("Yes");
+            else Console.WriteLine("No");
+
+        }
+    }
+}
